695487930908a12b345af19806b3efb072a05b79
gisele <medina.g.contact@gmail.com>
2026-04-12 22:06:02 -0300

    feat: add typedispatcher in generatetemplate

--- js/template.js
+++ js/template.js
@@ -1,5 +1,83 @@
-export const generateTemplate = (type, data) => {
-	if (!data || data.trim().length === 0) return "";
+import { formatArtist } from "./formatting";
 
-	return `Inspirado en ${data}`;
+const generateReinterpretation = ({ artist, work } = {}) => {
+	console.log("entro1", typeof artist);
+
+	if (typeof artist !== "string" || artist.trim().length === 0) return "";
+
+	console.log("entro2");
+	let message = `Reinterpretacion de `;
+	message +=
+		typeof work === "string" && work.trim().length > 0
+			? `obra "${work}" de ${artist}`
+			: `obra de ${artist}`;
+
+	console.log("message", message);
+	return message;
+};
+
+
+
+const generateTribute = ({ artist, work } = {}) => {
+	console.log("entro1", typeof artist);
+
+	if (typeof artist !== "string" || artist.trim().length === 0) return "";
+
+	console.log("entro2");
+	let message = `Homenaje a `;
+	message +=
+		typeof work === "string" && work.trim().length > 0
+			? `obra "${work}" de ${artist}`
+			: `${artist}`;
+
+	console.log("message", message);
+	return message;
+};
+
+const generateReference = ({ artist, work } = {}) => {
+	console.log("entro1", typeof artist);
+
+	if (typeof artist !== "string" || artist.trim().length === 0) return "";
+
+	console.log("entro2");
+	let message = `Referencia en `;
+	message +=
+		typeof work === "string" && work.trim().length > 0
+			? `obra "${work}" de ${artist}`
+			: `obra de ${artist}`;
+
+	console.log("message", message);
+	return message;
+};
+
+const generateInspiration = ({ artist, work } = {}) => {
+	console.log("entro1", typeof artist);
+
+	if (typeof artist !== "string" || artist.trim().length === 0) return "";
+
+	console.log("entro2");
+	let message = `Inspirado en `;
+	message +=
+		typeof work === "string" && work.trim().length > 0
+			? `obra "${work}" de ${artist}`
+			: `${artist}`;
+
+	console.log("message", message);
+	return message;
+};
+const typeDispatcher = {
+	inspiration: generateInspiration,
+	reference: generateReference,
+	tribute: generateTribute,
+	reinterpretation: generateReinterpretation,
+};
+export const generateTemplate = (type, { artist, work } = {}) => {
+	if (!type || typeof artist !== "string" || artist.trim().length === 0)
+		return "";
+	const formatAuthor = formatArtist(artist);
+	const normalizedType = type.toLowerCase();
+	const handler = typeDispatcher[normalizedType];
+	console.log("handler:::::", handler);
+	if (!handler) return "";
+	return handler({ artist: formatAuthor ,work});
 };
--- test/template.test.js
+++ test/template.test.js
@@ -1,9 +1,9 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test} from "vitest";
 import { generateTemplate } from "../js/template";
 
-describe("generateTemplate ", () => {
+describe("generateTemplate", () => {
 	test("return inspiration template", () => {
-		expect(generateTemplate("inspiration", "Emilia Bertolé")).toBe(
+		expect(generateTemplate("inspiration", {artist:"Emilia Bertolé"})).toBe(
 			"Inspirado en Emilia Bertolé",
 		);
 	});
@@ -11,8 +11,41 @@ describe("generateTemplate ", () => {
 		expect(generateTemplate("inspiration")).toBe("");
 	});
 	test("return inspiration formatting", () => {
-		expect(generateTemplate("reference", "emilia bertolé")).toBe(
+		expect(generateTemplate("inspiration", {artist:"emilia bertolé"})).toBe(
 			`Inspirado en Emilia Bertolé`,
 		);
+	});
+	test("return empty for empty args", () => {
+		expect(generateTemplate()).toBe("");
+	});
+
+	test("return empty for  wrong type", () => {
+		expect(generateTemplate("wrong", {artist:"Emilia Bertolé"})).toBe("");
+	});
+	test("return reference template", () => {
+		expect(generateTemplate("reference", {artist:"Emilia Bertolé"})).toBe(
+			"Referencia en obra de Emilia Bertolé",
+		);
+	});
+	test("return empty with reference and data empty", () => {
+		expect(generateTemplate("reference", " ")).toBe("");
+	});
+	test("return tribute template ", () => {
+		expect(generateTemplate("tribute", {artist:"Emilia Bertolé"})).toBe(
+			"Homenaje a Emilia Bertolé",
+		);
+	});
+	test("return reinterpretation template ", () => {
+		expect(generateTemplate("reinterpretation", {artist:"Emilia Bertolé"})).toBe(
+			"Reinterpretacion de obra de Emilia Bertolé",
+		);
+	});
+	test("return inspiration with artist and artist's work", () => {
+		expect(
+			generateTemplate("inspiration", {
+				artist: "Emilia Bertolé",
+				work: "Retrato de mi madre",
+			}),
+		).toBe(`Inspirado en obra "Retrato de mi madre" de Emilia Bertolé`);
 	});
 });
