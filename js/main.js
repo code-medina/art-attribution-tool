ba0b4081dd439789632b841f00e05617765d33da
gisele <medina.g.contact@gmail.com>
2026-04-17 23:20:28 -0300

    feat:add basic theme dark

--- js/main.js
+++ js/main.js
@@ -9,3 +9,49 @@ const templateTextarea = document.getElementById("template-button");
 
 form.addEventListener("submit", handleSubmit);
 templateTextarea.addEventListener("click", handleClickCopyTemplate);
+
+
+
+//theme
+const buttonTheme = document.getElementById("toggle-theme");
+if (buttonTheme) {
+    buttonTheme.addEventListener("click", e => {
+        console.log("click button theme ")
+        e.preventDefault();
+        const savedTheme = localStorage.getItem("theme");
+        if (!savedTheme || savedTheme == "light") {
+            localStorage.setItem("theme", "dark");
+            applyTheme("dark");
+            console.log("dark")
+            return;
+
+        }
+        console.log("light")
+        localStorage.setItem("theme", "light");
+        applyTheme("light")
+
+
+    })
+}
+const applyTheme = (theme) => {
+    if (theme) {
+        document.documentElement.setAttribute("data-theme", theme);
+        return;
+    }
+    document.documentElement.removeAttribute("data-theme");
+
+}
+
+const initTheme = () => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+        applyTheme(savedTheme);
+        return;
+    }
+    applyTheme(null);
+}
+//change in system preference
+const media = window.matchMedia("(prefers-color-scheme: dark)");
+media.addEventListener("change", e => {
+    if (!localStorage.getItem("theme")) { applyTheme(null) }
+})
