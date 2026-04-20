1943f2ad889be739fda6145ee1007fa3cd018850
gisele <medina.g.contact@gmail.com>
2026-04-19 23:03:07 -0300

    feat: add hidden attribute and input event listener

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import { handleClickCopyTemplate } from "./dialog.js";
-import { handleSubmit } from "./form.js";
+import { handleInput, handleSubmit } from "./form.js";
 import { handlerToggleTheme, initTheme } from "./theme.js";
 
 console.log(import.meta.url);
 //form
 const form = document.getElementById("form-template");
 form.addEventListener("submit", handleSubmit);
-
+form.querySelector("#work")?.addEventListener("input",handleInput);
 // dialog copy clipboard
 const templateTextarea = document.getElementById("template-button");
 templateTextarea.addEventListener("click", handleClickCopyTemplate);
